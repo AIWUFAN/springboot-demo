9dde22f6f1dd90055d0219508ef5ba9af2698755
AIWUFAN <git@github.com:AIWUFAN>
2019-09-08 17:09:28 +0800

    增加了bubble sort

--- src/main/java/com/test/jichu/HelloWorld.java
+++ src/main/java/com/test/jichu/HelloWorld.java
@@ -3,6 +3,7 @@ package com.test.jichu;
 import org.apache.log4j.Logger;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class HelloWorld {
@@ -17,8 +18,33 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         //sayHello("哈哈");
-        System.out.println("111\n22");
-        System.out.println("111\n22");
+//        System.out.println("111\n22");
+//        System.out.println("111\n22");
+        bubbleeTest();
     }
+
+    /**
+     *  冒泡
+     */
+    public static void bubbleeTest(){
+        int[] arr={2,1,5,0,3};
+        for (int i=0;i<arr.length-1;i++) {
+            for (int j=0;j<arr.length-i-1;j++) {
+                if (arr[j]>arr[j+1]) {
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+
+
+        }
+        Arrays.stream(arr).forEach(System.out::println);
+
+
+    }
+
+
+
 
 }
