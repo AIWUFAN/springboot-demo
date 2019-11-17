b5264a8feb0ba3c372311311826e5fb053f8dc28
AIWUFAN <git@github.com:AIWUFAN>
2019-11-17 16:18:16 +0800

    add test

--- src/main/java/com/test/controller/ControllerTest.java
+++ src/main/java/com/test/controller/ControllerTest.java
@@ -1,13 +1,32 @@
 package com.test.controller;
 
+import com.test.jichu.HelloService;
+import com.test.jichu.HelloWorld;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
 
 @RestController
 public class ControllerTest {
 
-    @RequestMapping("/test")
-    public  String index(){
-        return "helloworld";
+    private static final Logger logger= LoggerFactory.getLogger(ControllerTest.class);
+
+   // @Resource
+  //  HelloService helloService;
+
+
+    public static void main(String[] args) {
+        HelloService helloService = new HelloService();
+
+        String s = helloService.sayHello();
+        logger.info("controller test method index :{} ", s );
     }
+
+
+
 }
