c0371e598577be0bd645d94fae10f59193375ba8
AIWUFAN <31462729+AIWUFAN@users.noreply.github.com>
2019-11-17 16:24:11 +0800

    Update SpringbootDemoApplication.java

--- src/main/java/com/test/SpringbootDemoApplication.java
+++ src/main/java/com/test/SpringbootDemoApplication.java
@@ -11,6 +11,7 @@ public class SpringbootDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
         System.out.println("1111");
+        System.out.println("1111");
     }
 
 }
