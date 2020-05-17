da3e3b3cd0537fe4302166d0e366fae39f944290
AIWUFAN <1210799018@qq.com>
2020-05-17 17:42:15 +0800

    add hello world

--- src/main/java/com/test/demo/BrithdayAge.java
+++ src/main/java/com/test/demo/BrithdayAge.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class BrithdayAge {
     public static void main(String[] args) throws ParseException{
         getAge(parse("1994-12-28"));
-        System.out.println("100");
+        System.out.println("helloworld");
         
     }
 
